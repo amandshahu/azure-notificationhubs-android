0100c4ecd54cdbf535b7d9adfc8103ccebfa9087
Dmitry Galiev <v-dmgali@microsoft.com>
2020-08-18 14:33:48 +0300

    Add ADM lib

--- notification-hubs-sdk/src/adm/java/com/microsoft/windowsazure/messaging/notificationhubs/ADMReceiver.java
+++ notification-hubs-sdk/src/adm/java/com/microsoft/windowsazure/messaging/notificationhubs/ADMReceiver.java
@@ -1,4 +1,38 @@
 package com.microsoft.windowsazure.messaging.notificationhubs;
 
-public class ADMReceiver {
+import android.content.Intent;
+
+import com.amazon.device.messaging.ADMMessageHandlerBase;
+
+public class ADMReceiver extends ADMMessageHandlerBase {
+
+    private final NotificationHub mHub;
+
+    protected ADMReceiver() {
+        this(ADMReceiver.class.toString(), NotificationHub.getInstance());
+    }
+
+    protected ADMReceiver(String className, NotificationHub hub) {
+        super(className);
+        mHub = hub;
+    }
+
+    @Override
+    protected void onMessage(Intent intent) {
+    }
+
+    @Override
+    protected void onRegistrationError(String s) {
+
+    }
+
+    @Override
+    protected void onRegistered(String s) {
+
+    }
+
+    @Override
+    protected void onUnregistered(String s) {
+
+    }
 }
